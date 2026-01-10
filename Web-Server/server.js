355daa2f8d0b3d91b21c307db37ce3a41b9e6ae7
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2026-01-10 14:46:43 +0700

    menambahkan url dan method pada server

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -3,7 +3,64 @@ const http = require("http");
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
   response.statusCode = 200;
-  response.end("<h1>Hello ini http server</h1>");
+  const { method, url } = request;
+
+  if (url === "/") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end("<h1>Ini halaman utama!!!</h1>");
+    } else {
+      response.statusCode = 400;
+      response.end(`Tidak dapat meresponse ${method}`);
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end("<h1>Ini halaman About!!!</h1>");
+    } else if (method === "POST") {
+      let body = [];
+      response.statusCode = 200;
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Hai, ${name}!</h1>`);
+      });
+    } else {
+      response.statusCode = 400;
+      response.end(`Tidak dapat meresponse ${method}`);
+    }
+  } else {
+    response.statusCode = 404;
+    response.end(`${url} belum tersedia`);
+  }
+
+  //   if (method === "GET") {
+  //     response.end("<h1>Dapat!!!</h1>");
+  //   }
+  //   if (method === "POST") {
+  //     let body = [];
+
+  //     request.on("data", (chunk) => {
+  //       body.push(chunk);
+  //     });
+
+  //     request.on("end", () => {
+  //       body = Buffer.concat(body).toString();
+  //       const { name } = JSON.parse(body);
+  //       response.end(`<h1>Hai, ${name}!</h1>`);
+  //     });
+  //   }
+
+  //   if (method === "PUT") {
+  //     response.end("<h1>Ambil!!!</h1>");
+  //   }
+  //   if (method === "DELETE") {
+  //     response.end("<h1>Hapus!!!</h1>");
+  //   }
 };
 
 const server = http.createServer(requestListener);
